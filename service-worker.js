4fd9dff054374871aec3916fc39da8f6d89a424f
Masfred26 <joey.masfred@gmail.com>
2025-04-06 07:37:27 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("averias-cache").then((cache) =>
-      cache.addAll(["/", "/index.html", "/main.js"])
-    )
-  );
-});
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+
